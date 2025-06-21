1d4e29f8f9e6a92c30f65a80234e374e305049fd
truongnt <truongnt@nal.vn>
2025-06-21 11:59:38 +0700

    first commit

---
+++ test.rb
@@ -0,0 +1,14 @@
+def fullname(first, last)
+  "#{first} #{last}"
+end
+
+def greet(first, last)
+  "Hello, #{fullname(first, last)}!"
+end
+
+"last first".split.each do |name|
+  puts greet(name, "Doe")
+end
+
+
+
