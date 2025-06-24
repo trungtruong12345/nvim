8df58b3060c4ec9689d47f4b34b3dd5fc07084ca
trungtruong12345 <trungtruong12345xt@gmail.com>
2025-06-24 17:01:13 +0700

    feat: reconfig neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,5 +9,15 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>fs", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,          -- 👈 Show hidden files
+          hide_dotfiles = false,   -- 👈 Don't hide .files
+          hide_gitignored = false, -- 👈 (optional) Show gitignored files
+        },
+      },
+    })
 	end,
 }
+
